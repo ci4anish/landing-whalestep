61142bb92b7285887df6bced75c4f76fcc273c70
Marysh <35464372+Marysh@users.noreply.github.com>
2019-08-05 22:36:24 +0300

    new sections, responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,89 @@
 $(document).ready(function () {
+
+    $('.hdr nav li').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active')
+    });
+
+    $('a[href^="#"]').on('click',function (){
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 20 - $('.hdr').innerHeight()
+        }, 1000);
+    });
+
+
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 100) {
+            $('.hdr').addClass('sticky');
+        }
+        else {
+            $('.hdr').removeClass('sticky')
+        }
+    });
+
+
     $('.tech-stack-logos').slick({
         infinite: true,
-        slidesToShow: 7,
+        slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        touchMove: true,
+        arrows: false,
+        responsive: [
+
+            {
+                breakpoint: 1170,
+                settings: {
+                    slidesToShow: 5
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 560,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
+
     });
 
-    $('#openPopup').on('click', function () {
-        openPopup2();
+    $('#openTopPopup, #openHdrPopup').on('click', function () {
+        openTopPopup();
     })
-    // $().magnificPopup({type:'inline', src: '.contact_popup'});
+    $('#openBtmPopup').on('click', function () {
+        openBtmPopup();
+    })
+
 
 });
 
-function openPopup2() {
+
+function openTopPopup() {
     $.magnificPopup.open({
         items: {
-            src: '.contact_popup'
+            src: '.top-popup'
+        },
+        type: 'inline'
+    });
+}
+function openBtmPopup() {
+    $.magnificPopup.open({
+        items: {
+            src: '.adr_popup'
         },
         type: 'inline'
     });
 }
 
 
-
-
 function initMap() {
+
     var marker = new google.maps.Marker({
 
         // Определяем позицию маркера
@@ -35,11 +92,11 @@ function initMap() {
         animation: google.maps.Animation.BOUNCE,
 
         title: 'Whale Step',
-        icon: ''
+        icon: './img/location-marker.png'
     });
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 49.827607, lng: 24.044427},
-        zoom: 8,
+        zoom: 10,
 
         styles: [
             {
@@ -47,7 +104,7 @@ function initMap() {
                 "elementType": "geometry",
                 "stylers": [
                     {
-                        "color": "c7e5fc"
+                        "color": "#c4e3fc"
                     },
                     {
                         "lightness": 17
@@ -71,7 +128,7 @@ function initMap() {
                 "elementType": "geometry.fill",
                 "stylers": [
                     {
-                        "color": "#ffffff"
+                        "color": "#e0e0e0"
                     },
                     {
                         "lightness": 17
@@ -83,7 +140,7 @@ function initMap() {
                 "elementType": "geometry.stroke",
                 "stylers": [
                     {
-                        "color": "#ffffff"
+                        "color": "#e0e0e0"
                     },
                     {
                         "lightness": 29
@@ -142,30 +199,13 @@ function initMap() {
                 ]
             },
             {
-                "elementType": "labels.text.stroke",
-                "stylers": [
-                    {
-                        "visibility": "on"
-                    },
-                    {
-                        "color": "#ffffff"
-                    },
-                    {
-                        "lightness": 16
-                    }
-                ]
-            },
-            {
                 "elementType": "labels.text.fill",
                 "stylers": [
                     {
                         "saturation": 36
                     },
                     {
-                        "color": "#333333"
-                    },
-                    {
-                        "lightness": 40
+                        "color": "#9d9d9d"
                     }
                 ]
             },
--- scss/README.md
+++
@@ -1,8 +0,0 @@
-структура вмісту головного scss файлу : 
-1. 
-. 
-2. fonts, variables, mixins, functions
-reset|reboot
-3. libs
-4. custom libs styles
-5. my styles
